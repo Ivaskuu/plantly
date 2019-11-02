aee4b80cfd20324e220f9e49b372a71d393269b6
Ray Li <ray.li.verified@gmail.com>
2019-11-02 14:57:03 -0500

    Update Zoomable Image Widget
    *Remove deprecated zoomable image widget.
    *Replace with flutter_advanced_networkimage.
    *Update Android files to build.

--- lib/pages/zoomable_image_page.dart
+++ lib/pages/zoomable_image_page.dart
@@ -1,52 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:zoomable_image/zoomable_image.dart';
+import 'package:flutter_advanced_networkimage/zoomable.dart';
 
-class ZoomableImagePage extends StatelessWidget
-{
+class ZoomableImagePage extends StatelessWidget {
   final String imgPath;
   ZoomableImagePage(this.imgPath);
 
   @override
-  Widget build(BuildContext context)
-  {
-    return new Scaffold
-    (
+  Widget build(BuildContext context) {
+    return new Scaffold(
       backgroundColor: Colors.transparent,
-      body: new SizedBox.expand
-      (
-        child: new Stack
-        (
-          children: <Widget>
-          [
-            new Align
-            (
+      body: new SizedBox.expand(
+        child: new Stack(
+          children: <Widget>[
+            new Align(
               alignment: Alignment.center,
-              child: new Hero
-              (
+              child: new Hero(
                 tag: imgPath,
-                child: new ZoomableImage(new AssetImage(imgPath), scale: 3.0),
+                child: ZoomableWidget(
+                  panLimit: 1.0,
+                  maxScale: 3.0,
+                  minScale: 1.0,
+                  singleFingerPan: true,
+                  multiFingersPan: false,
+                  enableRotate: false,
+                  child: Image(
+                    image: AssetImage(imgPath),
+                  ),
+                  zoomSteps: 3,
+                ),
               ),
             ),
-            new Align
-            (
+            new Align(
               alignment: Alignment.topCenter,
-              child: new Column
-              (
+              child: new Column(
                 // I need to add a column to set the MainAxisSize to min,
                 // otherwise the appbar takes all the screen and the image is no more clickable
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
-                children: <Widget>
-                [
-                  new AppBar
-                  (
+                children: <Widget>[
+                  new AppBar(
                     elevation: 0.0,
                     backgroundColor: Colors.transparent,
-                    leading: new Container(), // Overrides the go back arrow icon button
-                    actions: <Widget>
-                    [
-                      new IconButton
-                      (
+                    leading:
+                        new Container(), // Overrides the go back arrow icon button
+                    actions: <Widget>[
+                      new IconButton(
                         onPressed: () => Navigator.of(context).pop(),
                         icon: new Icon(Icons.close, color: Colors.white),
                       ),
